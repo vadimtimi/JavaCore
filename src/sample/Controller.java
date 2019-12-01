37a2fc87329dd96d7171a6e6c03252f47f9913fe
VadimTimofeev <timofeev@iskra-kkm.ru>
2019-12-01 19:42:20 +0300

    - Домашнее задание 04 (готово для проверки)

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,6 +4,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
+
+import javax.management.loading.MLetContent;
 
 public class Controller {
     @FXML
@@ -12,7 +17,28 @@ public class Controller {
     @FXML
     private TextField textArea;
 
+    private void sendMessage() {
+        if(!textArea.getText().isEmpty()) {
+            textChat.appendText(textArea.getText() + "\n");
+            textArea.setText("");
+        }else {
+            String toastMsg = "Нет данных для отправки...";
+            int toastMsgTime = 3500; //3.5 seconds
+            int fadeInTime = 500; //0.5 seconds
+            int fadeOutTime= 500; //0.5 seconds
+            Stage stage = (Stage)textArea.getScene().getWindow();
+            Toast.makeText(stage, toastMsg, toastMsgTime, fadeInTime, fadeOutTime);
+        }
+    }
+
     public void ClickOnSend(ActionEvent actionEvent) {
-        textChat.appendText(textArea.getText() + "\n");
+        sendMessage();
+    }
+
+    public void handleEnterPressed(KeyEvent keyEvent) {
+        if (keyEvent.getCode() == KeyCode.ENTER) {
+            // Нажата клашиша ENTER
+            sendMessage();
+        }
     }
 }
